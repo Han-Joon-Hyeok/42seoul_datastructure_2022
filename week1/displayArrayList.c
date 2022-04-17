7a21137ebf5300de36c914534cac3414ae73a653
joonhyuk.han <virq@naver.com>
2022-04-17 17:14:19 +0900

    Create displayArrayList.c

---
+++ week1/displayArrayList.c
@@ -0,0 +1,23 @@
+#include "arraylist.h"
+#include <stdio.h>
+
+void displayArrayList(ArrayList* pList)
+{
+	int	max;
+	int	cur;
+	ArrayListNode	*arr;
+
+	max = pList->maxElementCount;
+	cur = pList->currentElementCount;
+	arr = pList->pElement;
+	
+	printf("max%d\n", max);
+	printf("cur%d\n", cur);
+	for(int i=0; i < cur; i++)
+	{
+		printf("%d", (arr + i)->data);
+		if (i != cur - 1)
+			printf(", ");
+	}	
+	printf("\n");
+}
