d38d8846d0dc65cd32cd9a9fab52f869235295f6
joonhyuk.han <virq@naver.com>
2022-04-17 09:49:34 +0900

    Modify addALElement.c
    feat: Change if condition when position is over maxCount and curr is under maxCount
    test: Add test case when position is under 0 and over array's max count

--- week1/addALElement.c
+++ week1/addALElement.c
@@ -7,14 +7,14 @@ int	addALElement(ArrayList *pList, int position, ArrayListNode element)
 	int	reps;
 	ArrayListNode *array;
 
-	if (isArrayListFull(pList))
+	if (isArrayListFull(pList) || position < 0)
 		return (FALSE);
 	else
 	{
 		curr = pList->currentElementCount;
 		max = pList->maxElementCount;
 		array = pList->pElement;
-		if (position > curr - 1 && position < max - 1)
+		if (position > curr - 1 && curr < max - 1)
 			*(array + curr) = element;
 		else if (position <= curr - 1)
 		{
@@ -38,13 +38,20 @@ int	main(void)
 	ArrayList	*arr;
 	ArrayListNode	node;
 	ArrayListNode	node2;
+	ArrayListNode	node3;
+	ArrayListNode	node4;
 
-	size = 2;
+
+	size = 5;
 	node.data = 1;
 	node2.data = 2;
+	node3.data = 3;
+	node4.data = 4;
 	arr = createArrayList(size);
-	addALElement(arr, 0, node);
-	addALElement(arr, 0, node2);
-	// [2, 1]
+
+	addALElement(arr, 8, node);   // [1]
+	addALElement(arr, 1, node2);  // [1, 2]
+	addALElement(arr, 0, node3);  // [3, 1, 2]
+	addALElement(arr, -9, node4); // [3, 1, 2]
 	return (0);
 }
