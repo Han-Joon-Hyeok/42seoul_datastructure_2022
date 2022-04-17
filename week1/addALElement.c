18b1db58d0051f28c59f9ebc386bb9ca6546f93a
joonhyuk.han <virq@naver.com>
2022-04-17 16:40:28 +0900

    Modify addALElement.c
    test: Remove main function

--- week1/addALElement.c
+++ week1/addALElement.c
@@ -31,27 +31,3 @@ int	addALElement(ArrayList *pList, int position, ArrayListNode element)
 		return (TRUE);
 	}
 }
-
-int	main(void)
-{
-	int		size;
-	ArrayList	*arr;
-	ArrayListNode	node;
-	ArrayListNode	node2;
-	ArrayListNode	node3;
-	ArrayListNode	node4;
-
-
-	size = 5;
-	node.data = 1;
-	node2.data = 2;
-	node3.data = 3;
-	node4.data = 4;
-	arr = createArrayList(size);
-
-	addALElement(arr, 8, node);   // [1]
-	addALElement(arr, 1, node2);  // [1, 2]
-	addALElement(arr, 0, node3);  // [3, 1, 2]
-	addALElement(arr, -9, node4); // [3, 1, 2]
-	return (0);
-}
