02b312f11ecca64e8d03ad415c0e78dd5ba46e44
joonhyuk.han <virq@naver.com>
2022-04-19 13:19:26 +0900

    Create getALElement.c

---
+++ week1/arrayList/getALElement.c
@@ -0,0 +1,18 @@
+#include "arraylist.h"
+
+ArrayListNode	*getALElement(ArrayList *pList, int position)
+{
+	int		curr;
+	int		max;
+	ArrayListNode	*arr;
+
+	curr = pList->currentElementCount;
+	max = pList->maxElementCount;
+	if (position > max - 1 || position < 0)
+		return (NULL);
+	else
+	{
+		arr = pList->pElement;
+		return (arr + position);
+	}
+}
