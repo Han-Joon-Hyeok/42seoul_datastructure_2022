8f046313abf98b259ff1151aa92626b100d0e7ae
Joonhyeok Han <joonhan@c1r9s1.42seoul.kr>
2022-04-25 16:34:03 +0900

    Modify checkBracketMatching.c
    refactor: Remove argument 'stack' from isOpenBracket and isCloseBracket

--- week2/checkBracketMathcing/checkBracketMathcing.c
+++ week2/checkBracketMathcing/checkBracketMathcing.c
@@ -1,16 +1,6 @@
-/*
-
-	- 스택이 비어있다면, 바로 추가
-	- 스택이 비어있지 않다면 다음과 같이 수행
-		- 스택의 가장 위에 있는 요소(TopElement)가 현재 들어온 괄호와 짝이 맞는지 확인
-		- 만약 같으면, pop(TopElement) 수행
-		- 같지 않으면 오류 발생
-
-*/
-
 #include "../linkedstack/linkedstack.h"
 
-int	isOpenBracket(LinkedStack *stack, StackNode node)
+int	isOpenBracket(StackNode node)
 {
 	char	c;
 
@@ -20,7 +10,7 @@ int	isOpenBracket(LinkedStack *stack, StackNode node)
 	return (FALSE);
 }
 
-int	isCloseBracket(LinkedStack *stack, StackNode node)
+int	isCloseBracket(StackNode node)
 {
 	char	c;
 
@@ -72,11 +62,11 @@ int checkBracketMatching(char *expr)
 	while (*(expr + i) != '\0' && result != FALSE)
 	{
 		node.data = *(expr + i);
-		if (isOpenBracket(stack, node))
+		if (isOpenBracket(node))
 		{
 			pushLS(stack, node);
 		}
-		else if (isCloseBracket(stack, node))
+		else if (isCloseBracket(node))
 		{
 			result = checkBracketPairs(stack, node);
 		}
@@ -87,12 +77,12 @@ int checkBracketMatching(char *expr)
 	return (result);
 }
 
-#include <stdio.h>
-int	main(int argc, char *argv[])
-{
-	if (argc >= 2)
-	{
-		printf("string: %s\n, result: %d", argv[1], checkBracketMatching(argv[1]));
-	}
-	return (0);
-}
+// #include <stdio.h>
+// int	main(int argc, char *argv[])
+// {
+// 	if (argc >= 2)
+// 	{
+// 		printf("string: %s\n, result: %d", argv[1], checkBracketMatching(argv[1]));
+// 	}
+// 	return (0);
+// }
