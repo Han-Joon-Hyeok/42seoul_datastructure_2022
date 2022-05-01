19c44b7605d682f29950278597879172ece51ea0
joonhyuk.han <virq@naver.com>
2022-05-01 18:12:56 +0900

    Modify arrayqueue.h
    feat: Change deleteArrayQueue's parameter data type to double pointer

--- week3/arrayqueue/arrayqueue.h
+++ week3/arrayqueue/arrayqueue.h
@@ -21,7 +21,7 @@ ArrayQueue* createArrayQueue(int maxElementCount);
 int enqueueAQ(ArrayQueue* pQueue, ArrayQueueNode element);
 ArrayQueueNode *dequeueAQ(ArrayQueue* pQueue);
 ArrayQueueNode *peekAQ(ArrayQueue* pQueue);
-void deleteArrayQueue(ArrayQueue* pQueue);
+void deleteArrayQueue(ArrayQueue** pQueue);
 int isArrayQueueFull(ArrayQueue* pQueue);
 int isArrayQueueEmpty(ArrayQueue* pQueue);
 
