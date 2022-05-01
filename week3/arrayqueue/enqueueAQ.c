5c2dcfa7c73a1cf56427e43c62a6550e0950e169
joonhyuk.han <virq@naver.com>
2022-05-01 17:56:36 +0900

    Modify enqueueAQ.c
    chore: Remove comments

--- week3/arrayqueue/enqueueAQ.c
+++ week3/arrayqueue/enqueueAQ.c
@@ -12,30 +12,3 @@ int enqueueAQ(ArrayQueue* pQueue, ArrayQueueNode element)
     }
     return (FALSE);
 }
-
-    /*
-    pQueue->pElement[pQueue->rear].data = element.data;
-    pQueue->currentElementCount++;
-    */
-
-    /*
-    max 3
-    count 3
-    0 1 2
-    A B c
-
-    0 1 2
-      b c
-    max 3
-    count 2
-    front 1
-    rear 2
-
-    0 1 2
-    D b c
-    max 3
-    count 2
-    front 1
-    rear 0
-
-    */
