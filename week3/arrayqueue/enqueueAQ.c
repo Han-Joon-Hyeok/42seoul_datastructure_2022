8312233964296e7443dc336de45bc2efc5fdb625
joonhyuk.han <virq@naver.com>
2022-05-01 18:35:06 +0900

    Modify enqueueAQ.c
    feat: Remove  allocating rear value

--- week3/arrayqueue/enqueueAQ.c
+++ week3/arrayqueue/enqueueAQ.c
@@ -1,12 +1,10 @@
 #include "arrayqueue.h"
-#include <stdio.h>
 
 int enqueueAQ(ArrayQueue* pQueue, ArrayQueueNode element)
 {
     if(pQueue && pQueue->currentElementCount < pQueue->maxElementCount)
     {
-        pQueue->rear = ++pQueue->rear % pQueue->maxElementCount;
-        pQueue->pElement[pQueue->rear] = element;
+        pQueue->pElement[++pQueue->rear] = element;
         pQueue->currentElementCount++;
         return (TRUE);
     }
