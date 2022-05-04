763d9c4de48390ac1ba9f21866272b2fa30aa9e7
Joonhyeok Han <joonhan@c5r1s1.42seoul.kr>
2022-05-04 20:05:35 +0900

    Update main.c
    test: Add processArrival function test
    test: Add changing customer status (not completed)

--- week3/simulation/main.c
+++ week3/simulation/main.c
@@ -3,11 +3,14 @@
 int main(void)
 {
     int i;
-    int t;
+    int t = 0;
     int processTime;
+    int serviceCount = 0;
+    int totalWaitTime = 0;
     int amount = 0;
     LinkedQueue *pArrivalQueue;
     LinkedQueue *pWaitQueue;
+    QueueNode   *currCustomer;
 
     pArrivalQueue = malloc(sizeof(LinkedQueue));
     pWaitQueue = malloc(sizeof(LinkedQueue));
@@ -23,13 +26,44 @@ int main(void)
         insertCustomer(i, processTime, pArrivalQueue);
     }
 
-    // Start processing
+    // 업무 시작
+    while (!isLinkedQueueEmpty(pArrivalQueue) || !isLinkedQueueEmpty(pWaitQueue))
+    {
+        processArrival(t, pArrivalQueue, pWaitQueue);
+        currCustomer = peekLQ(pWaitQueue);
+        
+        if (currCustomer->data.status == arrival) // 현재 서비스 중인 고객이 없으면 새로운 고객 서비스 시작
+        {
+            currCustomer->data.status = start;
+            currCustomer->data.startTime = t;
+            currCustomer->data.endTime += processTime;
+        }
+        else if (currCustomer->data.status == end) // 현재 서비스 중인 고객이 모두 업무를 마치면 Wait 큐에서 삭제
+        {
+            dequeueLQ(pWaitQueue);
+            processServiceNodeStart(t, pWaitQueue);
+            currCustomer = peekLQ(pWaitQueue);
+            currCustomer->data.endTime += t;
+            currCustomer->data.startTime = t;
+        }
 
-    // while (!isLinkedQueueEmpty(pWaitQueue))
-    // {
+        // 고객의 상태를 변경을 담당
+        // 현재 상태를 end 로 변경
+        if (currCustomer->data.startTime + currCustomer->data.processTime == t)
+        {
 
-    //     t += 1;
-    // }
+        }
+        // 현재 상태를 end 로 변경
+        if (currCustomer->data.startTime + currCustomer->data.recent  == currCustomer->data.endTime)
+        {
+            currCustomer->data.status = end;
+        }
+
+        // 고객의 서비스 시작과 종료를 담당
+            // processServiceNodeEnd(t, peekLQ(pWaitQueue), &serviceCount, &totalWaitTime);
+        t += 1;
+        currCustomer->data.recent++;
+    }
 
     // for (nasabah *n = nasabahs; n; n=n->next) {
     //     servedByCS(n, cs1, cs2);
