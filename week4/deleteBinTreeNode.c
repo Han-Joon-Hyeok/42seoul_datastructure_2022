4aa50471a603fe43592e21d455f05e9ada1cc2fa
joonhyuk.han <virq@naver.com>
2022-05-11 17:20:51 +0900

    Modify deleteBinTreeNode.c
    refactor: Add removing node's left child, right child

--- week4/deleteBinTreeNode.c
+++ week4/deleteBinTreeNode.c
@@ -4,6 +4,7 @@ void deleteBinTreeNode(BinTreeNode* pNode)
 {
 	if (pNode == NULL)
 		return ;
-	// pNode->pLeftChild = NULL; // should?
+	pNode->pLeftChild = NULL;
+	pNode->pRightChild = NULL;
 	free(pNode);
 }
