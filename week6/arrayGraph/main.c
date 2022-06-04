6dc89f0c86bc45b2a44fdc0ab7d4409a8eb51041
joonhyuk.han <virq@naver.com>
2022-06-04 22:08:51 +0900

    Modify main.c
    test: Add floyd function
    test: Add dijkstra function

--- week6/arrayGraph/main.c
+++ week6/arrayGraph/main.c
@@ -19,22 +19,59 @@ int main(void)
 
 */
 
-    // ugraph = createArrayGraph(4);
+    ugraph = createArrayGraph(5);
 
     // displayArrayGraph(ugraph);
 
-    // addVertexAG(ugraph, 0);
-    // addVertexAG(ugraph, 1);
-    // addVertexAG(ugraph, 2);
-    // addVertexAG(ugraph, 3);
+    addVertexAG(ugraph, 0);
+    addVertexAG(ugraph, 1);
+    addVertexAG(ugraph, 2);
+    addVertexAG(ugraph, 3);
+    addVertexAG(ugraph, 4);
 
     // addEdgeAG(ugraph, 0, 1);
     // addEdgeAG(ugraph, 0, 3);
     // addEdgeAG(ugraph, 1, 2);
     // addEdgeAG(ugraph, 1, 3);
-    // displayArrayGraph(ugraph);
+    addEdgewithWeightAG(ugraph, 0, 1, 2);
+    addEdgewithWeightAG(ugraph, 1, 2, 3);
+    addEdgewithWeightAG(ugraph, 1, 4, 1);
+    addEdgewithWeightAG(ugraph, 2, 3, 5);
+    addEdgewithWeightAG(ugraph, 3, 4, 4);
+
+    displayArrayGraph(ugraph);
+
     // arrayGraphDfs(ugraph);
     // arrayGraphBfs(ugraph);
+    
+    /*
+        ugraph witdh 
+        {
+            {0, 2, 0, 0, 0},
+            {2, 0, 3, 0, 1},
+            {0, 3, 0, 5, 0},
+            {0, 0, 5, 0, 4},
+            {0, 1, 0, 4, 0}
+        }
+        플로이드 알고리즘 최단 경로
+        [
+            [0, 2, 5, 7, 3],
+            [2, 0, 3, 5, 1],
+            [5, 3, 0, 5, 4],
+            [7, 5, 5, 0, 4],
+            [3, 1, 4, 4, 0]
+        ]
+
+    */
+
+    // floyd(ugraph);
+
+    /*
+        다익스트라 알고리즘 최단 경로 (0번 노드 기준)
+        dist = [0, 2, 5, 7, 3]
+    */
+
+    dijkstra(ugraph);
 
 /*
 
@@ -48,23 +85,23 @@ int main(void)
     ]
 
 */
-    dgraph = createArrayDirectedGraph(4);
+    // dgraph = createArrayDirectedGraph(4);
 
     // displayArrayGraph(dgraph);
 
-    addVertexAG(dgraph, 0);
-    addVertexAG(dgraph, 1);
-    addVertexAG(dgraph, 2);
-    addVertexAG(dgraph, 3);
+    // addVertexAG(dgraph, 0);
+    // addVertexAG(dgraph, 1);
+    // addVertexAG(dgraph, 2);
+    // addVertexAG(dgraph, 3);
 
-    addEdgeAG(dgraph, 0, 1);
-    addEdgeAG(dgraph, 1, 0);
-    addEdgeAG(dgraph, 1, 3);
-    addEdgeAG(dgraph, 3, 1);
-    addEdgeAG(dgraph, 3, 2);
-    displayArrayGraph(dgraph);
+    // addEdgeAG(dgraph, 0, 1);
+    // addEdgeAG(dgraph, 1, 0);
+    // addEdgeAG(dgraph, 1, 3);
+    // addEdgeAG(dgraph, 3, 1);
+    // addEdgeAG(dgraph, 3, 2);
+    // displayArrayGraph(dgraph);
     // arrayGraphDfs(dgraph);
-    arrayGraphBfs(dgraph);
+    // arrayGraphBfs(dgraph);
     // deleteArrayGraph(ugraph);
     // deleteArrayGraph(dgraph);
     return (0);
